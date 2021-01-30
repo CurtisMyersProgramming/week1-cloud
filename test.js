32a8c17b161808dcc6d137ed28772e84a9301c86
CurtisMyersProgramming <curtismyers25@gmail.com>
2021-01-30 03:09:00 +0000

    comments added to test.js

--- test.js
+++ test.js
@@ -1,11 +1,13 @@
-constexpress=require('express')
-constapp=express()
-constport=3000
+const express=require('express') // creates a const to the express framework
+const app=express() //creates an instance of the express restful service
+const port=3000 // creates a const with the port number for the service to listen on
 
+//method creates a hook on the server for get requets in the root directory
 app.get('/',(req,res)=>{
     res.send('Hello Virtual Machine!')
 })
 
+//method binds to the port specified in port const then console log when done 
 app.listen(port,()=>{
     console.log(`Express Application  listening at port 3000`)
 })
